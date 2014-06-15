56ede331466707fde32a373be4f8c9222db834c3
Sidhavratha <sidhavratha@gmail.com>
2014-06-15 19:18:26 +0530

    Initial commit

---
+++ src/test/java/GenomeMapReduceTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Sidhavratha on 15/6/14.
+ */
+public class GenomeMapReduceTest {
+
+    @Test
+    public void testGenomeMapReduce()
+    {
+        //Assert.fail("Intentional fail");
+    }
+}
